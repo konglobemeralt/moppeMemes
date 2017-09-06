e54be14ec30a3afacdd9f04e10c7690e6dd15cd9
Konglobemeralt <jesper.blidkvist@live.se>
2017-09-06 14:57:20 +0200

    Fixed some text issues

--- textGenerator/memeTextAdder.py
+++ textGenerator/memeTextAdder.py
@@ -5,8 +5,8 @@ from PIL import ImageDraw
 img = Image.open("images/test.jpg")
 draw = ImageDraw.Draw(img)
 
-font = ImageFont.truetype("Fonts/HelveticaBold.ttf", 16)
+font = ImageFont.truetype("Fonts/HelveticaBold.ttf", 90)
 
-draw.text((100,100), "Vroom, Vroom, nu kor vi!",(255,0,0),font=font)
+draw.text((100,100), "Vroom, Vroom, nu kor vi!",(255,255,255),font=font)
 img.save("dankMoppeMeme.jpg")
 
