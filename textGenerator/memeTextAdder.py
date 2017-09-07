9e650c027f5bd9f46f79061a95fd8c0d8ad56dd9
Konglobemeralt <jesper.blidkvist@live.se>
2017-09-07 15:34:06 +0200

    Testing generating many memes

--- textGenerator/memeTextAdder.py
+++ textGenerator/memeTextAdder.py
@@ -27,24 +27,38 @@ font = ImageFont.truetype("Fonts/HelveticaBold.ttf", 90)
 
 widthImg, heightImg = img.size
 
-text1 = text_model.make_short_sentence(60).rstrip()
-text2 = text_model.make_short_sentence(60).rstrip()
 
-margin = offset = widthImg* 0.05
-for line in textwrap.wrap(text1.upper(), width=19):
-    draw.text((margin, offset), line, font=font, fill="#ffffff")
-    offset += font.getsize(line)[1]
+for i in range(0, 1000):
+    try:
+        text1 = text_model.make_short_sentence(60).rstrip()
+    except AttributeError:
+        print "..."
+    try:
+        text2 = text_model.make_short_sentence(60).rstrip()
+    except AttributeError:
+        print "..."
+        
+    margin = offset = widthImg* 0.05
+    for line in textwrap.wrap(text1.upper(), width=19):
+        try:
+            draw.text((margin, offset), line, font=font, fill="#ffffff")
+        except TypeError:
+            print "Some error, ignoring line"
+        offset += font.getsize(line)[1]
 
-margin = widthImg* 0.05
-offset = heightImg * 0.80
-for line in textwrap.wrap(text2.upper(), width=19):
-    draw.text((margin, offset), line, font=font, fill="#ffffff")
-    offset += font.getsize(line)[1]
+    margin = widthImg* 0.05
+    offset = heightImg * 0.80
+    for line in textwrap.wrap(text2.upper(), width=19):
+        try:
+            draw.text((margin, offset), line, font=font, fill="#ffffff")
+        except TypeError:
+            print "Some error, ignoring line"
+        offset += font.getsize(line)[1]
 
 
-##draw.text((width* 0.10, height*0.05), text1.upper(), (255,255,255),font=font)
-##draw.text((width* 0.10, height*0.80), text2.upper(), (255,255,255),font=font)
+    ##draw.text((width* 0.10, height*0.05), text1.upper(), (255,255,255),font=font)
+    ##draw.text((width* 0.10, height*0.80), text2.upper(), (255,255,255),font=font)
 
 
-img.save("dankMoppeMeme.jpg")
+    img.save("GeneratedMemes/dankMoppeMemeGenerated" + str(i) + ".jpg")
 
