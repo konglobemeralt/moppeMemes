ae5d56b1be20c6d86eeb51fe114157eba740ab65
Konglobemeralt <jesper.blidkvist@live.se>
2017-09-07 15:15:08 +0200

    Added functional meming and example memes

--- textGenerator/memeTextAdder.py
+++ textGenerator/memeTextAdder.py
@@ -2,11 +2,49 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-img = Image.open("images/test.jpg")
+import time, sys
+import random
+import markovify
+import traceback
+import textwrap
+
+
+
+random.seed()
+
+counter = 0 
+argfile = 'redditMoppeMemesCombined.txt'
+    
+with open(argfile) as f:
+	text = f.read()
+        # Build the model.
+	text_model = markovify.Text(text)
+
+img = Image.open("images/meme1.jpg")
 draw = ImageDraw.Draw(img)
 
 font = ImageFont.truetype("Fonts/HelveticaBold.ttf", 90)
 
-draw.text((100,100), "Vroom, Vroom, nu kor vi!",(255,255,255),font=font)
+widthImg, heightImg = img.size
+
+text1 = text_model.make_short_sentence(60).rstrip()
+text2 = text_model.make_short_sentence(60).rstrip()
+
+margin = offset = widthImg* 0.05
+for line in textwrap.wrap(text1.upper(), width=19):
+    draw.text((margin, offset), line, font=font, fill="#ffffff")
+    offset += font.getsize(line)[1]
+
+margin = widthImg* 0.05
+offset = heightImg * 0.80
+for line in textwrap.wrap(text2.upper(), width=19):
+    draw.text((margin, offset), line, font=font, fill="#ffffff")
+    offset += font.getsize(line)[1]
+
+
+##draw.text((width* 0.10, height*0.05), text1.upper(), (255,255,255),font=font)
+##draw.text((width* 0.10, height*0.80), text2.upper(), (255,255,255),font=font)
+
+
 img.save("dankMoppeMeme.jpg")
 
--- textGenerator/moppeMemeGenerator.py
+++ textGenerator/moppeMemeGenerator.py
@@ -6,15 +6,15 @@ import traceback
 random.seed()
 
 counter = 0 
-argfile = 'moppeMemes.txt'
+argfile = 'redditMoppeMemesCombined.txt'
     
 with open(argfile) as f:
 	text = f.read()
         # Build the model.
 	text_model = markovify.Text(text)
 
-while counter < 100:  
-    text = text_model.make_short_sentence(140)
+while counter < 10:  
+    text = text_model.make_short_sentence(60)
     print(text)
     counter += 1
 
